def0dbbbfbb1cef73b2a9ecc6d5531d7cffd7746
tusharbindal16 <60348928+tusharbindal16@users.noreply.github.com>
2020-01-28 15:30:40 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,18 +1,18 @@
 <?php
 $servername = "35.223.220.76";
 $username = "newr";
-$password = "password";
+$dbpass = "password";
 $dbname = "testdb";
 $email=$_POST["email1"];
-$password=$_POST["pass2"];
+$pass=$_POST["pass2"];
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $dbpass, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "INSERT INTO login (email,password)
-VALUES ('$email','$password')";
+VALUES ('$email','$pass')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
