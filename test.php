5a87f857cf8d2dda0d4a7c3370710970c605fb05
tusharbindal16 <60348928+tusharbindal16@users.noreply.github.com>
2020-01-29 13:46:27 +0530

    Update test.php

--- test.php
+++ test.php
@@ -10,7 +10,7 @@ $conn = new mysqli($servername, $username, $dbpass, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT Email FROM register WHERE Email='".$email."'";
+$sql = "SELECT Email,Pass FROM register WHERE Email='".$email."'" AND Pass='".$pass."'";
 $result = $conn->query($sql);
 if($result->num_rows >= 1) {
     echo "Login Successfully";
